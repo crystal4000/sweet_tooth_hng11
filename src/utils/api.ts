5faad17be2a359f1afc4ec9c1abf033c0b96ca93
Tania <38540885+crystal4000@users.noreply.github.com>
2024-07-12 14:03:18 -0500

    API

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-//const baseApiUrl: string = process.env.REACT_APP_BASE_API_URL ?? "";
+const baseApiUrl: string = process.env.REACT_APP_BASE_API_URL ?? "";
 const apiKey: string = process.env.REACT_APP_API_KEY ?? "";
 const appId: string = process.env.REACT_APP_APP_ID ?? "";
 const organizationId: string = process.env.REACT_APP_ORGANIZATION_ID ?? "";
@@ -8,7 +8,7 @@ const organizationId: string = process.env.REACT_APP_ORGANIZATION_ID ?? "";
 export const productsApi = createApi({
   reducerPath: "productsApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3000/api", // This will be proxied to 'https://api.timbu.cloud'
+    baseUrl: baseApiUrl, // This will be proxied to 'https://api.timbu.cloud'
   }),
   endpoints: (builder) => ({
     getProducts: builder.query({
